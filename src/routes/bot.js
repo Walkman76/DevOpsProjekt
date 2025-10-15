c74a05f0496d3fc537ad7a24c3e7938c77f40cdc
arcaadser <149475007+arcaadser@users.noreply.github.com>
2025-10-15 11:26:47 +0300

    Add route to bot

--- src/routes/bot.js
+++ src/routes/bot.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
+app.get('/bot', (req, res) => {
 
     let msg = `<?xml version="1.0" encoding="UTF-8"?>
     <Response>
