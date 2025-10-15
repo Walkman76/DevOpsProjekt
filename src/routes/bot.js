e44575edb230ee2e3d6e753f91ca000c137ff366
arcaadser <149475007+arcaadser@users.noreply.github.com>
2025-10-15 14:22:58 +0300

    Fix bot, add REST Client tests

--- src/routes/bot.js
+++ src/routes/bot.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const router = express.Router()
 
-const app = express()
-const port = process.env.PORT || 8080
+router.get('/', (req, res) => {
+    try {
+        console.log('Bot svarar!')
+        const msg = `<?xml version="1.0" encoding="UTF-8"?>
+        <Response>
+            <Say>Have you tried turning it off and on again?</Say>
+        </Response>`
+        res.set("Content-Type", "text/xml")
+        res.send(msg)
 
-app.get('/bot', (req, res) => {
-
-    let msg = `<?xml version="1.0" encoding="UTF-8"?>
-    <Response>
-        <Say>Have you tried turning it off and on again?</Say>
-    </Response>`
-        
-    res.set("Content-Type:", "text/xml")
-    res.send(msg)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ msg: "Error" })
+    }
 })
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express()
 require('dotenv').config()
+
+const app = express()
 const PORT = process.env.PORT || 8080
 
 console.log(`Node.js ${process.version}`)
@@ -14,12 +15,6 @@ app.get('/', (req, res) => {
 const botRouter = require('./routes/bot')
 app.use('/bot', botRouter)
 
-
 app.listen(PORT, () => {
-    try {
-        console.log(`Running on http://localhost:${PORT}`)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-    
+    console.log(`Running on ${process.env.URL_BOT_API}:${PORT}`)   
 })
